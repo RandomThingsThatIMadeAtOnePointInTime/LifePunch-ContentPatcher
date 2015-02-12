3cb2a49db8b6d39b4338bf9b2832f0c6040f41fe
Scarsz <truescarsz@gmail.com>
2015-02-11 20:39:40 -0800

    Add v1

---
+++ LifePunch Content Autopatcher/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace LifePunch_Content_Autopatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.selectionBox = new System.Windows.Forms.ComboBox();
+            this.actionButton = new System.Windows.Forms.Button();
+            this.folder = new System.Windows.Forms.FolderBrowserDialog();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.folderStatus = new System.Windows.Forms.TextBox();
+            this.chooseFolder = new System.Windows.Forms.Button();
+            this.logBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // selectionBox
+            // 
+            this.selectionBox.AutoCompleteCustomSource.AddRange(new string[] {
+            "Content (models/hats/etc)",
+            "Bunnyhop Maps",
+            "Deathrun Maps",
+            "Gungame Maps",
+            "Jailbreak Maps"});
+            this.selectionBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.selectionBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.selectionBox.Enabled = false;
+            this.selectionBox.FormattingEnabled = true;
+            this.selectionBox.Items.AddRange(new object[] {
+            "Content",
+            "Bunnyhop Maps",
+            "Deathrun Maps",
+            "Gungame Maps",
+            "Jailbreak Maps"});
+            this.selectionBox.Location = new System.Drawing.Point(340, 12);
+            this.selectionBox.Name = "selectionBox";
+            this.selectionBox.Size = new System.Drawing.Size(150, 21);
+            this.selectionBox.TabIndex = 3;
+            this.selectionBox.SelectedIndexChanged += new System.EventHandler(this.selectionBox_SelectedIndexChanged);
+            // 
+            // actionButton
+            // 
+            this.actionButton.Enabled = false;
+            this.actionButton.Location = new System.Drawing.Point(496, 12);
+            this.actionButton.Name = "actionButton";
+            this.actionButton.Size = new System.Drawing.Size(73, 21);
+            this.actionButton.TabIndex = 4;
+            this.actionButton.Text = "<- Choose";
+            this.actionButton.UseVisualStyleBackColor = true;
+            this.actionButton.Click += new System.EventHandler(this.actionButton_Click);
+            // 
+            // folder
+            // 
+            this.folder.Description = "Select the topmost folder of GarrysMod.                               A \"hl2.exe\"" +
+    " program should be here.";
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(12, 12);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(242, 20);
+            this.pathBox.TabIndex = 0;
+            this.pathBox.Text = "Select your topmost GarrysMod folder";
+            this.pathBox.TextChanged += new System.EventHandler(this.pathBox_TextChanged);
+            // 
+            // folderStatus
+            // 
+            this.folderStatus.BackColor = System.Drawing.SystemColors.Control;
+            this.folderStatus.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.folderStatus.Location = new System.Drawing.Point(294, 15);
+            this.folderStatus.Name = "folderStatus";
+            this.folderStatus.ReadOnly = true;
+            this.folderStatus.Size = new System.Drawing.Size(40, 13);
+            this.folderStatus.TabIndex = 2;
+            this.folderStatus.Text = "SELECT";
+            // 
+            // chooseFolder
+            // 
+            this.chooseFolder.Location = new System.Drawing.Point(252, 12);
+            this.chooseFolder.Name = "chooseFolder";
+            this.chooseFolder.Size = new System.Drawing.Size(36, 20);
+            this.chooseFolder.TabIndex = 1;
+            this.chooseFolder.Text = "...";
+            this.chooseFolder.UseVisualStyleBackColor = true;
+            this.chooseFolder.Click += new System.EventHandler(this.chooseFolder_Click);
+            // 
+            // logBox
+            // 
+            this.logBox.BackColor = System.Drawing.SystemColors.WindowText;
+            this.logBox.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.logBox.FormattingEnabled = true;
+            this.logBox.Location = new System.Drawing.Point(12, 39);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(557, 186);
+            this.logBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 240);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.chooseFolder);
+            this.Controls.Add(this.folderStatus);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.actionButton);
+            this.Controls.Add(this.selectionBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 279);
+            this.MinimumSize = new System.Drawing.Size(600, 279);
+            this.Name = "Form1";
+            this.Text = "LifePunch Content Patcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox selectionBox;
+        private System.Windows.Forms.Button actionButton;
+        private System.Windows.Forms.FolderBrowserDialog folder;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.TextBox folderStatus;
+        private System.Windows.Forms.Button chooseFolder;
+        private System.Windows.Forms.ListBox logBox;
+    }
+}
+
---
+++ LifePunch Content Autopatcher/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LifePunch_Content_Autopatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool checkIfGmodFolderisOK(string folder)
+        {
+            if (File.Exists(folder + "\\hl2.exe") && Directory.Exists(folder + "\\garrysmod"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        void openProgram(string program, string args)
+        {
+            Process process = new Process();
+            try
+            {
+                process.StartInfo.FileName = program;
+                process.StartInfo.Arguments = args;
+                process.Start();
+                process.WaitForExit();
+                process.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error launching: " + program + "\nInner: " + e.InnerException + "\nSource: " + e.Source + "\nString" + e.ToString());
+            }
+        }
+        void showFolderDialog()
+        {
+            DialogResult result = folder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                pathBox.Text = folder.SelectedPath;
+            }
+        }
+        bool installPackage(string name)
+        {
+            string installDir = pathBox.Text + "\\garrysmod\\addons\\LifePunch - " + name;
+            logBox.Items.Add("Installation directory: \"" + installDir + "\"");
+            if (Directory.Exists(installDir))
+            {
+                Directory.Delete(installDir);
+            }
+
+            using (WebClient client = new WebClient())
+            {
+                #region Downloading zips
+                logBox.Items.Add("Downloading zip file \"" + name + "\", please be patient");
+
+                if (name == "Content"){
+                    client.DownloadFile("https://doc-0o-b0-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/rg67106f1g0p3cf63umqceukc9ak8g0k/1423706400000/10026425688678393106/*/0B5fwVeQ4kXnfN3lHOWpTMEVaVlE?e=download", "LifePunch - Content.zip");
+                } else if (name == "Bunnyhop Maps"){
+                    client.DownloadFile("https://doc-0o-b0-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/bihh6mqnfo35orvrg3seuepo107501f1/1423706400000/10026425688678393106/*/0B5fwVeQ4kXnfdFdBcE9zZzBxM00?e=download", "LifePunch - Bunnyhop Maps.zip");
+                } else if (name == "Deathrun Maps"){
+                    client.DownloadFile("https://doc-0o-b0-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/rmadm7a05e5svvjc0e5h71v4kh8ojsmp/1423706400000/10026425688678393106/*/0B5fwVeQ4kXnfUXlBa3poaTc4YzQ?e=download", "LifePunch - Deathrun Maps.zip");
+                } else if (name == "Gungame Maps"){
+                    client.DownloadFile("https://doc-04-b0-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/pbmnconshrv2ive0foqsa50ita57dimj/1423706400000/10026425688678393106/*/0B5fwVeQ4kXnfeWdBSkJRdm5Zb1k?e=download", "LifePunch - Gungame Maps.zip");
+                } else if (name == "Jailbreak Maps"){
+                    client.DownloadFile("https://doc-08-b0-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/mmv07dk1viiauok2bfqk4ggqlf96qbd5/1423706400000/10026425688678393106/*/0B5fwVeQ4kXnfUGplMXhVTi03RDg?e=download", "LifePunch - Jailbreak Maps.zip");
+                }
+                #endregion
+                logBox.Items.Add("Downloading unzip");
+                client.DownloadFile("https://github.com/Scarsz/windowsgameservers/blob/master/unzip.exe?raw=true", "unzip.exe");
+            }
+            logBox.Items.Add("Extracting files, please be patient");
+            openProgram("unzip.exe", "-o \"LifePunch - " + name + ".zip\" -d \"" + installDir + "\" -x INSTRUCTIONS.txt");
+            if (Directory.Exists(installDir))
+            {
+                logBox.Items.Add("Extraction finished");
+            }
+            else
+            {
+                logBox.Items.Add("Extraction did not complete successfully");
+                logBox.Items.Add("Unknown reason");
+            }
+            logBox.Items.Add("Deleting unzip.exe");
+            File.Delete("unzip.exe");
+            logBox.Items.Add("Deleting package \"LifePunch - " + name + "\".zip");
+            File.Delete("LifePunch - " + name + ".zip");
+            return true;
+        }
+
+        private void pathBox_TextChanged(object sender, EventArgs e)
+        {
+            if (checkIfGmodFolderisOK(pathBox.Text))
+            {
+                folderStatus.ForeColor = Color.Green;
+                folderStatus.Text = "GOOD";
+                actionButton.Enabled = true;
+                selectionBox.Enabled = true;
+            }
+            else
+            {
+                folderStatus.ForeColor = Color.Red;
+                folderStatus.Text = "ERROR";
+                actionButton.Enabled = false;
+                selectionBox.Enabled = false;
+            }
+        }
+        private void chooseFolder_Click(object sender, EventArgs e)
+        {
+            showFolderDialog();
+        }
+        private void selectionBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (selectionBox.Text != "Content" && 
+                selectionBox.Text != "Bunnyhop Maps" &&
+                selectionBox.Text != "Deathrun Maps" &&
+                selectionBox.Text != "Gungame Maps" &&
+                selectionBox.Text != "Jailbreak Maps")
+            {
+                MessageBox.Show("Invalid package");
+            }
+            else
+            {
+                logBox.Items.Add("Package \"" + selectionBox.Text + "\" selected");
+                actionButton.Enabled = true;
+                if (folderStatus.Text == "GOOD")
+                {
+                    if (Directory.Exists(pathBox.Text + "\\garrysmod\\addons\\LifePunch - " + selectionBox.Text))
+                    {
+                        actionButton.Text = "Reinstall";
+                        logBox.Items.Add("Package \"" + selectionBox.Text + "\" is already installed, available for reinstall");
+                    }
+                    else
+                    {
+                        actionButton.Text = "Install";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Select your topmost GarrysMod folder first.");
+                }
+            }
+        }
+        private void actionButton_Click(object sender, EventArgs e)
+        {
+            if (actionButton.Text != "<- Choose")
+            {
+                actionButton.Enabled = false;
+                chooseFolder.Enabled = false;
+                pathBox.Enabled = false;
+                selectionBox.Enabled = false;
+                actionButton.Text = "Installing";
+
+                logBox.Items.Add("Beginning installation of \"" + selectionBox.Text + "\"");
+                installPackage(selectionBox.Text);
+                logBox.Items.Add("Installation completed");
+
+                actionButton.Enabled = true;
+                chooseFolder.Enabled = true;
+                pathBox.Enabled = true;
+                selectionBox.Enabled = true;
+                actionButton.Text = "<- Choose";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Update notification
+            int version = 1;
+            using (WebClient client = new WebClient())
+            {
+                int latestVersion = Convert.ToInt32(client.DownloadString("https://raw.githubusercontent.com/Scarsz/LifePunch-ContentPatcher/master/version"));
+                if (version < latestVersion)
+                {
+                    MessageBox.Show("This version of LPCAP is outdated, please download the newest version.");
+                    System.Diagnostics.Process.Start("https://github.com/Scarsz/LifePunch-ContentPatcher/releases");
+                }
+            }
+        }
+    }
+}
